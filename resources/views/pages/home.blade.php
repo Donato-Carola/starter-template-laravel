881ed8f5f63833ae352067905585b434bd89d818
Donato-Carola <don94@live.it>
2024-01-31 16:27:02 +0100

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+
+
+@section('main-content')
+
+<h1>
+
+    home
+</h1>
+
+
+
+@endsection
+
+@section('script-content')
+
+<script>
+    console.log('ciao, prova lo script in una pagina')
+</script>
+@endsection
---
+++ resources/views/pages/secondpage.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title','titolo seconda pagina')
+
+
+
+@section('main-content')
+
+<h1>
+
+   second page
+</h1>
+
+
+@endsection
